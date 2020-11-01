2d6e7c46bb13f63228ad545706bf0d3ed453381a
kuenhwi <kueniyam@naver.com>
2020-11-02 00:21:49 +0900

    fix: home 화면에 별이 제대로 나오지 않던 현상 수정

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,24 +4,22 @@ import {Link} from "react-router-dom";
 
 const Home = () => {
   return (
-    <>
+    <div className="homePage">
       <div className="drMartens title">Dr.Martens</div>
-      <div className="topTwinkle">
-        <img className="twinkle" src="/images/twinkle.png"/>
-      </div>
-      <div className="middle">
-        <div className="middleItems">
-          <div className="description">나에게 어울리는 닥터마틴은?</div>
-          <Link to="/questions">
-            <div className="testButton">닥터마틴 유형 알아보기</div>
-          </Link>
+      <div className="middleItems">
+        <div className="topTwinkle">
+          <img className="twinkle" src="/images/twinkle.png"/>
+        </div>
+        <div className="description">나에게 어울리는 닥터마틴은?</div>
+        <Link to="/questions">
+          <div className="testButton">닥터마틴 유형 알아보기</div>
+        </Link>
+        <div className="bottomTwinkle">
+          <img className="twinkle" src="/images/twinkle.png"/>
         </div>
       </div>
-      <div className="bottomTwinkle">
-        <img className="twinkle" src="/images/twinkle.png"/>
-      </div>
       <div className="typeTest title">TYPE TEST</div>
-    </>
+    </div>
   );
 };
 
